404befcb9bacc8fa52139ed1597ff7e46279df4f
Jeremy <b51448@freescale.com>
2014-08-15 10:57:28 +0800

    commit not message box

--- src/Build_station/buildConfig/buildConfigHost.py
+++ src/Build_station/buildConfig/buildConfigHost.py
@@ -1,6 +1,7 @@
 from PyQt4 import QtGui,QtCore 
 import sys,os,shutil
 from buildConfigUi import Ui_buildConfig
+from buildQuitTipsHost import buildQuit
 
 file_path = os.path.dirname(os.path.abspath(__file__))
 main_path = os.path.join(file_path,'../')
@@ -12,9 +13,9 @@ sys.path.append(lib_path)
 
 from getconfig import Getconfig
 
-config_file_backup = file_path + '/config.xml'
-config_file = file_path + '/config_operate.xml'
-#shutil.copyfile(config_file_backup,config_file)
+config_file_backup = config_path + 'config.xml'
+config_file = config_path + 'config_operate.xml'
+shutil.copyfile(config_file_backup,config_file)
 
 
 class buildStationConfig(QtGui.QMainWindow):
@@ -23,6 +24,8 @@ class buildStationConfig(QtGui.QMainWindow):
         self.buildConfigWin = Ui_buildConfig()
         self.buildConfigWin.setupUi(self)
         self.setFixedSize(self.width(),self.height())
+        
+        self.buildTips = buildQuit()
 
 #*********** MenuBar, ToolBar, StatusBar ************         
         self.menuBar = self.menuBar()
@@ -32,13 +35,16 @@ class buildStationConfig(QtGui.QMainWindow):
 #********************* Actions **********************        
         self.saveAct = QtGui.QAction(QtGui.QIcon("pics/save.png"),"&Save",self)
         self.saveAct.setShortcut("Ctrl+S")
-        self.saveAct.setStatusTip("Save the Configuration File")
+        self.saveAct.setStatusTip("Save the Configuration File and Quit the Configuration")
         self.saveAct.whatsThis()
+        self.connect(self.saveAct, QtCore.SIGNAL("triggered()"),self.close)
         
         self.quitAct = QtGui.QAction(QtGui.QIcon("pics/quit.png"),"Quit",self)
         self.quitAct.setShortcut("Ctrl+Q")
-        self.quitAct.setStatusTip("Quit the Configuration")
+        self.quitAct.setStatusTip("Quit the Configuration without saving the File")
         self.saveAct.whatsThis()
+        self.connect(self.quitAct, QtCore.SIGNAL("triggered()"),self.buildTips.buildQuitTipsShow)
+        self.connect(self.buildTips.buildQuitWin.quitYesButton,QtCore.SIGNAL("clicked()"),self.close)
         
 
 #****************add Menus and Actions**************
